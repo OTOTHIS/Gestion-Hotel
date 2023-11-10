5a6401075fc4bc4a361acdd58966370ac6284bf2
othmane <144280859+OthmaneAssadi212@users.noreply.github.com>
2023-11-11 00:19:47 +0100

    befor liste de reservation

--- src/data/reservationList.jsx
+++ src/data/reservationList.jsx
@@ -1,24 +1,59 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
+
 import { useEffect, useState } from "react";
-import { DateNow, formatedDate } from "../partials/functions";
+import ChambresListe from "./chambreList";
 
 const ReservationList = () => {
   const [reservation, SetReservation] = useState([]);
-
+  const { changeReserveStatus } = ChambresListe();
   // Load data from local storage on component mount
   useEffect(() => {
     const storedreservation = JSON.parse(localStorage.getItem("reservations"));
-    if ( storedreservation && storedreservation.length !== 0  ) {
-        SetReservation(storedreservation);
+    if (storedreservation && storedreservation.length !== 0) {
+      SetReservation(storedreservation);
     }
   }, []);
 
 
+  useEffect(() => {
+    const currentDate = new Date().toISOString();
+
+    const latestReservations = {};
+
+    reservation.forEach((reservation) => {
+      const currentNumero = reservation.numero;
+
+      if (
+        !latestReservations[currentNumero] ||
+        new Date(reservation.fin) >
+          new Date(latestReservations[currentNumero].fin)
+      ) {
+        latestReservations[currentNumero] = reservation;
+      }
+    });
+
+    const updatedReservations = Object.values(latestReservations).filter(
+      (reservation) => new Date(reservation.fin) < new Date(currentDate)
+    );
+
+    if (updatedReservations.length === 0) {
+      console.log("No reservation ends now");
+    } else {
+      console.log(updatedReservations);
+      updatedReservations.forEach((item) =>
+        changeReserveStatus(item.numero, false)
+      );
+
+      if (JSON.stringify(updatedReservations) !== JSON.stringify(reservation)) {
+        SetReservation(updatedReservations);
+      }
+    }
+  }, [reservation, changeReserveStatus]);
 
 
 
 
-  // Save data to local storage whenever reservation changes
   useEffect(() => {
     localStorage.setItem("reservations", JSON.stringify(reservation));
   }, [reservation]);
@@ -26,4 +61,3 @@ const ReservationList = () => {
 };
 
 export default ReservationList;
-
